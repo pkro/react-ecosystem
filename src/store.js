378a3ba01bd19e0056ab85a823b4f95e4ed105d9
pkro <pkrombach@web.de>
2020-04-30 10:29:46 +0200

    persist state in localstorage using redux-persist

--- src/store.js
+++ src/store.js
@@ -1,8 +1,19 @@
 import { createStore, combineReducers } from 'redux';
+import { persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import autoMergeLevel2 from 'redux-persist/lib/stateReconciler/autoMergeLevel2';
 import { todos } from './todos/reducers';
 
 const reducers = { todos };
 
 const rootReducer = combineReducers(reducers);
 
-export const configureStore = () => createStore(rootReducer);
+const persistConfig = {
+  key: 'root',
+  storage, // defaults to localStorage on web
+  stateReconciler: autoMergeLevel2,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const configureStore = () => createStore(persistedReducer);
